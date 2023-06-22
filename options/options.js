ef7948d28d5351bda98a0a2922ed560c9c2e918b
Crowdbotics <team@crowdbotics.com>
2023-06-22 12:39:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nedia-lead-crm-41673",
+  url: "https://nedia-lead-crm-41673.botics.co",
+  api: "https://nedia-lead-crm-41673.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
